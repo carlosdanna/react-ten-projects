ad3fedd6799c29e775985361e1c1abbe38d1a4cd
Carlos Danna <carlos.danna@gmail.com>
2016-04-27 16:04:54 -0400

    Question list and Question Component added

--- reactquizz/src/components/App.jsx
+++ reactquizz/src/components/App.jsx
@@ -1,11 +1,113 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import QuestionList from './quiz/QuestionList.jsx';
 
 class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            questions: [
+                {
+                    id: 1,
+                    text: 'What is your name?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'Michael'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Brad'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Stephen'
+                        },
+                        {
+                            id: 'd',
+                            text: 'Dan'
+                        }
+                    ],
+                    correct: 'b'
+                },
+                {
+                    id: 2,
+                    text: 'What is your mothers name?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'Racheal'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Sara'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Sue'
+                        },
+                        {
+                            id: 'd',
+                            text: 'Dona'
+                        }
+                    ],
+                    correct: 'c'
+                },
+                {
+                    id: 3,
+                    text: 'What is your fathers name?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'Boby'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Brad'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Stephen'
+                        },
+                        {
+                            id: 'd',
+                            text: 'Michael'
+                        }
+                    ],
+                    correct: 'd'
+                },
+                {
+                    id: 4,
+                    text: 'What is your friends name?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'Michael'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Brad'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Stephen'
+                        },
+                        {
+                            id: 'd',
+                            text: 'Michael'
+                        }
+                    ],
+                    correct: 'a'
+                },
+            ],
+            score: 0,
+            current: 1
+        }
+    }
     render(){
         return (
             <div>
-                APP
+                <QuestionList {...this.state}/>
             </div>
         )
     }
