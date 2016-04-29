60bbccd4bdf7e4723a155981ca9ddaa67a1d35bc
Carlos Danna <carlos.danna@gmail.com>
2016-04-29 14:28:50 -0400

    Infofinder boilerplate and basic components implemented

---
+++ 07-infofinder/src/js/components/SearchResults.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStore');
+
+
+
+var SearchResults = React.createClass({
+
+    render: function(){
+
+        return(
+            <div>
+                SearchResults
+            </div>
+        )
+    },
+
+})
+
+module.exports = SearchResults;
