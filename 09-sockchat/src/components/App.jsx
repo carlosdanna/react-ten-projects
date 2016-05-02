bc43a83a04bff6535ec03dde98a5159ca61fdbfb
Carlos Danna <carlos.danna@gmail.com>
2016-05-02 14:09:28 -0400

    basic project 09 layout

---
+++ 09-sockchat/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return (
+            <div>This is my App</div>
+        )
+    }
+}
+
+export default App;
