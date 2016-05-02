63f9c89d45582bb3f277274623d6a00b5beed72d
Carlos Danna <carlos.danna@gmail.com>
2016-05-02 10:12:35 -0400

    scafolding project 8

---
+++ 08-videolibrary/README.md
@@ -0,0 +1,3 @@
+#React Base Project
+
+This a base project made on react which I use as a base for most of my project done on React
---
+++ 08-videolibrary/src/js/components/App.js
@@ -0,0 +1,37 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStore');
+var AddForm = require('./AddForm');
+
+function getAppState(){
+    return {
+
+    }
+}
+
+var App = React.createClass({
+    getInitialState: function(){
+        return getAppState();
+    },
+    componentDidMount: function(){
+        AppStore.addChangeListener(this._onChange);
+    },
+    componentWillUnmount: function(){
+        AppStore.removeChangeListener(this._onChange);
+    },
+    render: function(){
+
+        return(
+            <div>
+                <AddForm></AddForm>
+            </div>
+        )
+    },
+
+    _onChange: function(){
+        this.setState(getAppState());
+    }
+
+})
+
+module.exports = App;
